dd894cff758148c4634bc21b6f896e8443c78734
Aditya Pandey <adityapandey@Adityas-MacBook-Air.local>
2021-08-17 16:53:57 +0530

    Testing new json decoder/encoder #1

--- executor.go
+++ executor.go
@@ -2,13 +2,14 @@ package dqlx
 
 import (
 	"context"
-	// "encoding/json"
+	"encoding/json"
 	"errors"
+	"log"
 	"reflect"
 	"time"
 
 	// "github.com/goccy/go-json"
-	jsoniter "github.com/json-iterator/go"
+	// jsoniter "github.com/json-iterator/go"
 
 	"github.com/dgraph-io/dgo/v210"
 	"github.com/dgraph-io/dgo/v210/protos/api"
@@ -75,6 +76,7 @@ func (executor OperationExecutor) ExecuteQueries(ctx context.Context, queries ..
 	}
 
 	query, variables, err := QueriesToDQL(queries...)
+	log.Println("Query:: ", query)
 	if err != nil {
 		return nil, err
 	}
@@ -210,7 +212,7 @@ func (executor OperationExecutor) toResponse(resp *api.Response, queries ...Quer
 func mutationData(mutation MutationBuilder) (updateData []byte, deleteData []byte, err error) {
 	var setDataBytes []byte
 	var deleteDataBytes []byte
-	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	// var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 	if mutation.setData != nil {
 		setBytes, err := json.Marshal(mutation.setData)
@@ -261,7 +263,7 @@ type Response struct {
 // into an interface value
 func (response Response) Unmarshal(value interface{}) error {
 	values := map[string]interface{}{}
-	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	// var json = jsoniter.ConfigCompatibleWithStandardLibrary
 	err := json.Unmarshal(response.Raw.Json, &values)
 
 	if err != nil {
